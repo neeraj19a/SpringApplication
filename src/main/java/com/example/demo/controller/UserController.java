2daacb7f3cea740379a231328a2eae43b25033a1
unknown <neeraj.bakhtani@gmail.com>
2020-03-15 14:50:09 +0800

    Spring Application

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    UserService userService;
+
+    @RequestMapping(value = "/all", method = RequestMethod.GET)
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @RequestMapping(value = "/adduser", method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public User addUser(@RequestBody User user) {
+        return this.userService.addUser(user);
+    }
+
+    //other controllers omitted for brevity
+
+}
---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,27 @@
+package com.example.demo.service;
+
+import com.example.demo.dao.UserDao;
+import com.example.demo.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    UserDao userDao;
+
+    public List<User> getAllUsers() {
+        return this.userDao.findAll();
+    }
+
+    public User addUser(User user) {
+        return this.userDao.save(user);
+    }
+
+    //other methods omitted for brevity
+}
